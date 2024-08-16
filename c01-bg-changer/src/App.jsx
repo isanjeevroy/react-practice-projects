0aac388ef3dae1225390c1f6a348add1023b91a9
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-08-16 16:17:20 +0530

    done bg changer

--- c01-bg-changer/src/App.jsx
+++ c01-bg-changer/src/App.jsx
@@ -1,7 +1,62 @@
+import { useState } from "react"
 
 function App() {
+
+  const [color, setColor] = useState("olive")
+
   return (
-    <h1>Hey </h1>
+    <div className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}>
+
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+
+          <button
+            onClick={() => setColor('red')}
+            className="px-4 outline-none py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: 'red' }}
+          >
+            Red
+          </button>
+
+          <button
+            onClick={() => setColor('green')}
+            className="px-4 outline-none py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: 'green' }}
+          >
+            Green
+          </button>
+
+          <button
+            onClick={() => setColor('black')}
+            className="px-4 outline-none py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: 'black' }}
+          >
+            Black
+          </button>
+
+          <button
+            onClick={() => setColor('purple')}
+            className="px-4 outline-none py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: 'purple' }}
+          >
+            Purple
+          </button>
+
+          <button
+            onClick={() => setColor('blue')}
+            className="px-4 outline-none py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: 'blue' }}
+          >
+            Blue
+          </button>
+
+          <button
+            onClick={() => setColor('orange')}
+            className="px-4 outline-none py-1 rounded-full text-white shadow-lg" style={{ backgroundColor: 'orange' }}
+          >
+            Orange
+          </button>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
